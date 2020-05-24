112174a16214c75c4475fba3883f2eac7fe95f54
Alex Whyatt <59887470+CptBarnacles@users.noreply.github.com>
2020-05-24 18:02:25 +0100

    progress#stable.2

--- app/models/image.rb
+++ app/models/image.rb
@@ -2,4 +2,8 @@ class Image < ApplicationRecord
   belongs_to :album
   has_one_attached :drawing
   has_rich_text :body
+
+	def drawing_f
+  		return self.drawing.variant(resize: "300x300!").processed
+ 	end
 end
