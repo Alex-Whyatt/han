ca9c4a08b9baad706b325214cf33fa22dd1456b4
Alex Whyatt <59887470+CptBarnacles@users.noreply.github.com>
2020-05-24 17:32:16 +0100

    progress#stable

--- app/models/album.rb
+++ app/models/album.rb
@@ -1,4 +1,8 @@
 class Album < ApplicationRecord
 	has_many :images, :dependent => :destroy
 	has_one_attached :thumbnail
+
+	def thumbnail_f
+  		return self.thumbnail.variant(resize: "300x300!").processed
+ 	end
 end
--- app/models/image.rb
+++ app/models/image.rb
@@ -2,8 +2,4 @@ class Image < ApplicationRecord
   belongs_to :album
   has_one_attached :drawing
   has_rich_text :body
-
-  def optimized_image(image,x,y)
-    return image.variant(resize_to_fill: [x, y]).processed
-  end
 end
