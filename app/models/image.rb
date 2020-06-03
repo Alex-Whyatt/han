76a81f8049a9dcbeac70e2d1548e9c6cc73bf177
Alex Whyatt <59887470+CptBarnacles@users.noreply.github.com>
2020-06-03 15:26:59 +0100

    mostly working

--- app/models/image.rb
+++ app/models/image.rb
@@ -4,7 +4,10 @@ class Image < ApplicationRecord
   has_rich_text :body
 
 	def drawing_f
-  		return self.drawing.variant(resize: "250x
-  			200!").processed
+  		return self.drawing.variant(resize: "250x200!").processed
+ 	end
+
+ 	def drawing_big
+  		return self.drawing.variant(resize: "400x400!").processed
  	end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -2,5 +2,6 @@ Rails.application.routes.draw do
   resources :images
   resources :albums, :has_many => :images
   get 'info/index'
+  get 'images/:id', to: 'images#index', as: 'backto'
   root 'info#index'
 end
