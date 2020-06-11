20f7aec80bd613c654326b07f7544aa97750355d
Alex Whyatt <59887470+CptBarnacles@users.noreply.github.com>
2020-06-11 15:08:57 +0100

    styling mostly done

--- app/models/image.rb
+++ app/models/image.rb
@@ -4,10 +4,18 @@ class Image < ApplicationRecord
   has_rich_text :body
 
 	def drawing_f
-  		return self.drawing.variant(resize: "400x400!").processed
+  		return self.drawing.variant(resize: "200x200!").processed
  	end
 
  	def drawing_big
   		return self.drawing.variant(resize: "400x400!").processed
  	end
+
+ 	def add_one
+ 		self.id + 1
+ 	end
+
+ 	def next_1
+ 		Image.offset(self.id).limit(1).order("id ASC").first
+  	end
 end
